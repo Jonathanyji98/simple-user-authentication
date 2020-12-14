642b9bef2338adb3fd35562aab575f0e2bb279cc
jonathan-inspiretek <jonathan@inspiretek.io>
2020-12-14 16:44:48 +1000

    WIP

--- user-authentication/src/app/payment-portal/payment-portal.component.ts
+++ user-authentication/src/app/payment-portal/payment-portal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { loadStripe } from '@stripe/stripe-js'; // this is typescript
 
+declare var require: any
 @Component({
   selector: 'app-payment-portal',
   templateUrl: './payment-portal.component.html',
@@ -10,14 +11,18 @@ export class PaymentPortalComponent implements OnInit {
 
   stripePromise = loadStripe('pk_test_51Ht0EFErANrRMY7ozGv1sRxdRdTHOIYaJNaHhDH16F8kQfvKRjxtXAqjiDf6yDiBEY7rztDdg7H0Ffw2tmWHnW1B0011a8EC29');
   amount: number = 1;
-  price: string;
-
+  // price: string;
+  // firebase = require('firebase-functions');
   constructor() { }
 
   ngOnInit() {
   }
 
   async checkout(price: string) {
+    // const getSessionId = this.firebase.functions().httpsCallable('getSessionId');
+    // getSessionId().then(function(result){
+    //   console.log(result);
+    // });
     const stripe = await this.stripePromise;
     const { error } = await stripe.redirectToCheckout({
       mode: 'subscription',  
@@ -32,15 +37,21 @@ export class PaymentPortalComponent implements OnInit {
   }
 
   checkoutBasicPrice(){
-    this.checkout(this.price = 'price_1HtOCwErANrRMY7o0yxjPW13');
+    console.log("TESTING CLOUD FUNCTION");
+    // this.checkout(this.price = 'price_1HtOCwErANrRMY7o0yxjPW13');
+    const getSessionId = this.firebase.functions().httpsCallable('getSessionId');
+    getSessionId().then(function(result){
+      console.log(result);
+    });
+    console.log("TESTING CLOUD FUNCTION 2");
   }
 
-  checkoutCommunityPrice(){
-    this.checkout(this.price = 'price_1HtOCwErANrRMY7ok8YqbS5U');
-  }
+  // checkoutCommunityPrice(){
+  //   this.checkout(this.price = 'price_1HtOCwErANrRMY7ok8YqbS5U');
+  // }
 
-  checkoutPerformancePrice(){
-    this.checkout(this.price = 'price_1HtOCxErANrRMY7ocgjLrlrj');
-  }
+  // checkoutPerformancePrice(){
+  //   this.checkout(this.price = 'price_1HtOCxErANrRMY7ocgjLrlrj');
+  // }
 
 }
